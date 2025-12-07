f42518d4aefb96389c8284b0d35a4b55e3f9fedf
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-07 18:22:54 +0200

    Add CustomerRepository interface extending JpaRepository

---
+++ src/main/java/com/example/ecommerce_api/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce_api.repository;
+
+import com.example.ecommerce_api.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
