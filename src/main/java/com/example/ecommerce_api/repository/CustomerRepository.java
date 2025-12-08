c36f42af5d6b6fda774f68ddfbd7442f33a435c1
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-08 13:59:02 +0200

    add existsByEmail() method to check if customer altready exists

--- src/main/java/com/example/ecommerce_api/repository/CustomerRepository.java
+++ src/main/java/com/example/ecommerce_api/repository/CustomerRepository.java
@@ -4,5 +4,5 @@ import com.example.ecommerce_api.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    boolean existsByEmail(String email);
 }
