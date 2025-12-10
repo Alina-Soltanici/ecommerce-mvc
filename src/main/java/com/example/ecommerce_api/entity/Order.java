715224b0f2f142908a7743166c9d40924cee05b5
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-10 07:29:48 +0200

    Create Order entity with ID, customer relation and order items list

--- src/main/java/com/example/ecommerce_api/entity/Order.java
+++ src/main/java/com/example/ecommerce_api/entity/Order.java
@@ -1,4 +1,31 @@
 package com.example.ecommerce_api.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Table(name = "orders", schema = "ecommerce")
+@NoArgsConstructor
+@Data
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id", nullable = false, unique = true, updatable = false )
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
 }
