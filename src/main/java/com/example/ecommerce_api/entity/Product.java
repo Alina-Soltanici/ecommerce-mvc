12fa8046bad9cb7f40f2de56b38be84ec11ffc38
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-11 11:29:53 +0200

    Add Product entity with Color and Size enums

---
+++ src/main/java/com/example/ecommerce_api/entity/Product.java
@@ -0,0 +1,31 @@
+package com.example.ecommerce_api.entity;
+
+import com.example.ecommerce_api.entity.enums.Color;
+import com.example.ecommerce_api.entity.enums.Size;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products", schema = "ecommerce")
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Color color;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "product_size")
+    private Size size;
+
+    private Double price;
+}
