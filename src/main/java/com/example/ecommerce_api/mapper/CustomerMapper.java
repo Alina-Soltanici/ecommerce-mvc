326ff0b812cd02a1e3f037e66c2ad1f2f6cfcdc7
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-11 23:09:10 +0200

    replace manual CustomerMapper with MapStruct mapper

--- src/main/java/com/example/ecommerce_api/mapper/CustomerMapper.java
+++ src/main/java/com/example/ecommerce_api/mapper/CustomerMapper.java
@@ -3,31 +3,15 @@ package com.example.ecommerce_api.mapper;
 import com.example.ecommerce_api.dto.CustomerRequest;
 import com.example.ecommerce_api.dto.CustomerResponse;
 import com.example.ecommerce_api.entity.Customer;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Component
-public class CustomerMapper {
-    //Customer -> CustomerResponse
-    public CustomerResponse toCustomerResponse(Customer customer) {
-        CustomerResponse response = new CustomerResponse();
-        response.setId(customer.getId());
-        response.setFirstName(customer.getFirstName());
-        response.setLastName(customer.getLastName());
-        response.setEmail(customer.getEmail());
-        response.setPhoneNumber(customer.getPhoneNumber());
-        response.setAddress(customer.getAddress());
-        return response;
-    }
 
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+    //Customer -> CustomerResponse
+    public CustomerResponse toDto(Customer customer);
 
     //CustomerRequest -> Customer
-    public Customer toCustomer(CustomerRequest customerRequest) {
-        Customer customer = new Customer();
-        customer.setFirstName(customerRequest.getFirstName());
-        customer.setLastName(customerRequest.getLastName());
-        customer.setEmail(customerRequest.getEmail());
-        customer.setPhoneNumber(customerRequest.getPhoneNumber());
-        customer.setAddress(customerRequest.getAddress());
-        return customer;
-    }
+    public Customer toEntity(CustomerRequest customerRequest);
 }
