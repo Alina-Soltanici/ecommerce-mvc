744f7660db93a218ea58bf55b96d9280ed983c57
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-11 21:32:45 +0200

    add repository for order items

--- src/main/java/com/example/ecommerce_api/repository/OrderRepository.java
+++ src/main/java/com/example/ecommerce_api/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.example.ecommerce_api.repository;
 
-public interface OrderRepository {
+import com.example.ecommerce_api.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
