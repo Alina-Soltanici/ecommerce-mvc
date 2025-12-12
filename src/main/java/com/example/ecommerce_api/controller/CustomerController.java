20a5a29c0587198e18804240716c67f17e35d709
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-12 13:59:25 +0200

    feat: add POST endpoint to create customer in CustomerController

--- src/main/java/com/example/ecommerce_api/controller/CustomerController.java
+++ src/main/java/com/example/ecommerce_api/controller/CustomerController.java
@@ -1,12 +1,25 @@
 package com.example.ecommerce_api.controller;
 
+import com.example.ecommerce_api.dto.CustomerRequest;
+import com.example.ecommerce_api.dto.CustomerResponse;
+import com.example.ecommerce_api.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerResponse> createCustomer(@RequestBody CustomerRequest request) {
+        CustomerResponse created = customerService.createCustomer(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+
 
 }
