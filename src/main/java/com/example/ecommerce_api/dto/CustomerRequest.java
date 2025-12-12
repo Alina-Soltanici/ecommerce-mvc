1806c041031d31f170929a9b8d0099dd4141b602
Alina <soltanicialina777@gmail.com>
2025-12-12 17:24:42 +0200

    Add @NotBlank, @Size, and @Email constraints to CustomerRequest fields

--- src/main/java/com/example/ecommerce_api/dto/CustomerRequest.java
+++ src/main/java/com/example/ecommerce_api/dto/CustomerRequest.java
@@ -1,12 +1,27 @@
 package com.example.ecommerce_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+
 
 @Data
 public class CustomerRequest {
+    @NotBlank(message = "First name is required")
+    @Size(min = 3, max = 50, message = "First name must be between 3 and 50 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 3, max = 30, message = "Last name must be between 3 and 30 characters")
     private String lastName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
     private String address;
 }
