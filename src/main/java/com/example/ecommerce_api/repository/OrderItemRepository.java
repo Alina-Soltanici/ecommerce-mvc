83a3f217a5c2810b21d2ab7904f2a03490be83ac
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-12 14:59:28 +0200

    fix: correct OrderItemRepository to extend JpaRepository<OrderItem, Long>

--- src/main/java/com/example/ecommerce_api/repository/OrderItemRepository.java
+++ src/main/java/com/example/ecommerce_api/repository/OrderItemRepository.java
@@ -1,9 +1,10 @@
 package com.example.ecommerce_api.repository;
 
+import com.example.ecommerce_api.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderItemRepository extends JpaRepository<OrderItemRepository, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
