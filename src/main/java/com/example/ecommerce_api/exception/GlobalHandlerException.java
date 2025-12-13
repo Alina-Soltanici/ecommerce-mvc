8897c215ab2af7d86797ac2cfdba4c5d2376812d
Alina <soltanicialina777@gmail.com>
2025-12-13 17:19:49 +0200

    Replace HashMap with ErrorResponse in GlobalExceptionHandler and add handlers for EmailAlreadyExists and PhoneAlreadyExists exceptions

--- src/main/java/com/example/ecommerce_api/exception/GlobalHandlerException.java
+++ src/main/java/com/example/ecommerce_api/exception/GlobalHandlerException.java
@@ -1,21 +1,40 @@
 package com.example.ecommerce_api.exception;
 
+import com.example.ecommerce_api.exception.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalHandlerException {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleResourceNotFound(ResourceNotFoundException resourceNotFoundException) {
-        HashMap<String, Object> errorMap = new HashMap<>();
-        errorMap.put("timestamp", LocalDateTime.now());
-        errorMap.put("status", HttpStatus.NOT_FOUND.value());
-        errorMap.put("error", "Not Found");
-        errorMap.put("message", resourceNotFoundException.getMessage());
-        return new ResponseEntity<> (errorMap, HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleResourceNotFound(ResourceNotFoundException resourceNotFoundException) {
+        return new ErrorResponse(
+                "RESOURCE_NOT_FOUND",
+                resourceNotFoundException.getMessage(),
+                LocalDateTime.now()
+        );
     }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleEmailExists(EmailAlreadyExistsException ex) {
+        return new ErrorResponse(
+                "EMAIL_ALREADY_EXISTS",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(PhoneAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handlePhoneExists(PhoneAlreadyExistsException ex) {
+        return new ErrorResponse(
+                "PHONE_ALREADY_EXISTS",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+    }
+
 }
