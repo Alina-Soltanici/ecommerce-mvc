99c6859f80e4a8ab86c717a67941ea838ed562ae
Alina <soltanicialina777@gmail.com>
2025-12-14 15:29:47 +0200

    Define ProductService interface contract

---
+++ src/main/java/com/example/ecommerce_api/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce_api.service;
+
+import com.example.ecommerce_api.dto.*;
+import java.util.List;
+
+public interface ProductService {
+    ProductResponse createProduct(ProductRequest productRequest);
+
+    ProductResponse getProduct(Long id);
+
+    List<ProductResponse> getProducts();
+
+    ProductResponse updateProduct(Long id, ProductRequest updated);
+
+    void deleteProduct(Long id);
+}
