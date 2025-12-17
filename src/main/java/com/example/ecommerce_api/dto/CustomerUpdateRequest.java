1088ad5aa5a7dc0b397142b8cce6e3361fd2725d
Alina <soltanicialina777@gmail.com>
2025-12-17 22:30:21 +0200

    refactor(customer): make CustomerUpdateRequest fields optional

--- src/main/java/com/example/ecommerce_api/dto/CustomerUpdateRequest.java
+++ src/main/java/com/example/ecommerce_api/dto/CustomerUpdateRequest.java
@@ -1,20 +1,16 @@
 package com.example.ecommerce_api.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerUpdateRequest {
-    @NotBlank(message = "First name is required")
     @Size(min = 3, max = 50, message = "First name must be between 3 and 50 characters")
     private String firstName;
 
-    @NotBlank(message = "Last name is required")
     @Size(min = 3, max = 30, message = "Last name must be between 3 and 30 characters")
     private String lastName;
 
-    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
     private String address;
 }
