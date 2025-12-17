d055208ee48119b4aa12d909f8e1630284418401
Alina <soltanicialina777@gmail.com>
2025-12-17 15:44:47 +0200

    chore: remove unnecessary blank line in CustomerService

--- src/main/java/com/example/ecommerce_api/service/CustomerService.java
+++ src/main/java/com/example/ecommerce_api/service/CustomerService.java
@@ -3,7 +3,6 @@ package com.example.ecommerce_api.service;
 import com.example.ecommerce_api.dto.CustomerRequest;
 import com.example.ecommerce_api.dto.CustomerResponse;
 import com.example.ecommerce_api.dto.CustomerUpdateRequest;
-
 import java.util.List;
 
 public interface CustomerService {
