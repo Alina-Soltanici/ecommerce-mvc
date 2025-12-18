94d946dbaa5dc187f745a9d99888b216f4269b9d
Alina <soltanicialina777@gmail.com>
2025-12-18 22:11:43 +0200

    Add GET endpoints to ProductController for all products and by ID

--- src/main/java/com/example/ecommerce_api/controller/ProductController.java
+++ src/main/java/com/example/ecommerce_api/controller/ProductController.java
@@ -8,10 +8,9 @@ import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -23,5 +22,18 @@ public class ProductController {
     public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest productRequest) {
         ProductResponse created = productService.createProduct(productRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getProducts() {
+        List<ProductResponse> products = productService.getProducts();
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+        ProductResponse product = productService.getProduct(id);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
     }
 }
